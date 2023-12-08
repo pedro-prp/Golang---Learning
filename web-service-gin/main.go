70894ef085152bb764edf13b4876a8875f7b9406
Pedro Rodrigues <peddroprp@gmail.com>
2023-12-08 14:11:58 -0300

    Create POST endpoint

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -27,9 +27,22 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJson to bind the receiced JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
